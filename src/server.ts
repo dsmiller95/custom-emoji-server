95bc3775de8231322596f69e4e85ccc2e0b6248d
Dan Miller <dan.miller@centare.com>
2019-07-28 18:39:39 -0500

    set up heroku configuration and simple connection to a postgresql DB

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,29 @@
 import express from 'express';
+import { Pool } from 'pg';
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+});
+
 app.get('/', (req, res) => res.send('Hello Emoji Users!!'));
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM test_table');
+        const results = {
+            results: result ? result.rows : null
+        };
+        res.send(results);
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send('Error ' + err);
+    }
+});
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
