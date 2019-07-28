094993b331cc958eff08034eb90b41ea692a005c
Dan Miller <dan.miller@centare.com>
2019-07-28 18:10:54 -0500

    configurable port

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello Emoji Users!!'));
 
-app.listen(80, () => console.log('Example app listening on port 80!'));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
