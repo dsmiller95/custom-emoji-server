3004627d95747931a2fb5c695ca226404726c437
Dan Miller <dan.miller@centare.com>
2019-07-28 17:59:01 -0500

    set up to host on port 80 by default

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello Emoji Users!!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(80, () => console.log('Example app listening on port 80!'));
